6f27276d7bf9e977fce1868bc995bc4e330c4687
[Maria] <[mariaclara00763@gmail.com]>
2025-05-20 16:25:57 -0300

    atualização do calculate.js

--- api/calculate.js
+++ api/calculate.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://calculadora-microservicos-zeta.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
